4dc8c1c14609eccd1dd6b10fd4da549842b7f33f
ChristianJehoshaphatS <christian.suryo@phiraka.com>
2026-06-08 20:37:47 +0700

    feat(sales): add payment_type field to SalesHeaderForm

--- apps/sales/forms.py
+++ apps/sales/forms.py
@@ -7,10 +7,11 @@ from .models import SalesHeader, SalesItem
 class SalesHeaderForm(forms.ModelForm):
     class Meta:
         model = SalesHeader
-        fields = ('tanggal', 'entitas_bisnis', 'payment_account', 'deskripsi')
+        fields = ('tanggal', 'entitas_bisnis', 'payment_account', 'payment_type', 'deskripsi')
         widgets = {
             'tanggal': forms.DateInput(attrs={'class': 'ni-input', 'type': 'date'}),
             'entitas_bisnis': forms.Select(attrs={'class': 'ni-input'}),
             'payment_account': forms.Select(attrs={'class': 'ni-input'}),
+            'payment_type': forms.Select(attrs={'class': 'ni-input', 'id': 'id_payment_type'}),
             'deskripsi': forms.Textarea(attrs={'class': 'ni-input', 'rows': 2}),
         }
