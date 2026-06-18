8a0c8c9f27a226f5636976077066c0ff71f3d060
ArutanDV <dantadwipayanastan@gmail.com>
2026-06-19 01:38:57 +0800

    fix(pajak): add default_auto_field and label to PajakConfig

--- apps/pajak/apps.py
+++ apps/pajak/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class PajakConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.pajak'
+    label = 'pajak'
     verbose_name = 'Pajak'
